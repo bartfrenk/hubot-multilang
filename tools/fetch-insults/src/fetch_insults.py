5fc1566b217379582ec6951986aa00530fb9b29a
Bart Frenk <bart.frenk@gmail.com>
2018-10-28 12:06:07 +0100

    Allow sync and async request fetching

--- tools/fetch-insults/src/fetch_insults.py
+++ tools/fetch-insults/src/fetch_insults.py
@@ -1,34 +1,40 @@
 import logging
-#import requests
-from requests import async
+import grequests
+import gevent.monkey
 from argparse import ArgumentParser
 from bs4 import BeautifulSoup
+gevent.monkey.patch_ssl()
+import requests
 
 log = logging.getLogger(__name__)
 
 URL = 'https://www.wowbagger.com/process.php'
 
+# def fetch_page():
+#     response = requests.get(URL)
+#     if response.status_code == 200:
+#         return response.content
+#     else:
+#         log.warn('Failed to fetch insult')
+#         return None
 
-def fetch_page():
-    response = requests.get(URL)
-    if response.status_code == 200:
-        return response.content
-    else:
-        log.warn('Failed to fetch insult')
-        return None
 
-def parse_insult(page):
+def parse_insult(page: str) -> str:
     html = BeautifulSoup(page, 'html.parser')
-    return html.findAll('span', {'class': 'customBig'})
+    segments = html.findAll('span', {'class': 'customBig'})
+    if segments:
+        return segments[0].string
 
 
-
-def new_parser():
+def new_parser() -> ArgumentParser:
     parser = ArgumentParser()
-    parser.add_argument(
-        '-n', metavar='<int>', type=int,
-        help="Number of insults to fetch",
-        default=1)
+    parser.add_argument('-n',
+                        metavar='<int>',
+                        required=True,
+                        type=int,
+                        help="Number of insults to fetch",
+                        default=1)
+    parser.add_argument('--async', action='store_true')
     return parser
 
 
@@ -37,20 +43,31 @@ def read_arguments():
     return parser.parse_args()
 
 
-def get_async(n, size):
-    rs = [async.get(URL) for _ in range(n)]
-    pages = [p for p in async.map(rs) if p.status_code == 200]
-    return pages
+def is_valid(resp: requests.Response) -> bool:
+    return hasattr(resp, "status_code") and resp.status_code == 200
 
+
+def get_async(n: int, *args, **kwargs):
+    rs = (grequests.get(URL) for _ in range(n))
+    responses = grequests.imap(rs, *args, **kwargs)
+    pages = (resp.content for resp in responses if is_valid(resp))
+    return (parse_insult(page) for page in pages)
+
+
+def get_sync(n: int):
+    for i in range(n):
+        resp = requests.get(URL)
+        if is_valid(resp):
+            yield parse_insult(resp.content)
+
+
+def main(n: int, async: bool=True):
+    insults = get_async(n, size=6) if async else get_sync(n)
+
+    for ins in insults:
+        print(ins)
 
 
 if __name__ == "__main__":
     arguments = read_arguments()
-    insults = []
-    get_async(arguments.n, 10)
-    # for i in range(arguments.n):
-    #     page = fetch_page()
-    #     if page:
-    #         parsed = parse_insult(page)
-    #         if len(parsed) > 0:
-    #             print(parsed[0].get_text())
+    main(arguments.n, arguments.async)
